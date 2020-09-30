1e82dcdaa29687b8e9c4ceb826ff12a4d501b5b3
danschae <danschae@gmail.com>
2020-09-30 18:47:04 +0000

    changes

--- express_server.js
+++ express_server.js
@@ -3,6 +3,9 @@ const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
+const bcrpyt = require("bcrypt");
+const salt = bcrpyt.genSaltSync(10);
 
 //global function for generating a random string for the shorturl
 function generateRandomString() {
@@ -16,6 +19,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 // can use cookie parsingdfsdfs
 app.use(cookieParser());
 
+//implementing cookiesession
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['iamasuperkeyandilikesongs', 'pouet pouet yes spaces are okay why not']
+}))
+
 app.set('view engine', 'ejs');
 
 //object containing all the saved urlsear
@@ -27,13 +37,23 @@ const urlDatabase = {
 //user informationd
 const users = {};
 
-//fudddnction to sort through emails
-const sortObj = (obj, email) => {
+//function to sort through emails
+const checkEmailExists = (obj, email) => {
   for (const key in obj) {
     if (obj[key].email === email) {
-      return false
+      return true
     } else {
-    return true
+    return false
+    }
+  }
+};
+
+const checkPassword = (obj, password) => {
+  for (const key in obj) {
+    if (bcrpyt.compareSync(password, obj[key].password) === true) {
+      return true;
+    } else {
+      return false;
     }
   }
 };
@@ -41,36 +61,74 @@ const sortObj = (obj, email) => {
 
 //get the user registration page
 app.get("/register", (req, res) => {
-  res.render("sign_in")
+  const templatVars = {
+    urls: urlDatabase,
+    username: req.cookies["user_id"],
+    users: users[req.cookies["user_id"]]
+  };
+  res.render("sign_in",templatVars)
+})
+//get to sign in page
+app.get("/sign_in", (req, res) => {
+  const templatVars = {
+    urls: urlDatabase,
+    username: req.cookies["user_id"],
+    users: users[req.cookies["user_id"]]
+  };
+  res.render("log_in",templatVars)
 })
 
 //registration post, need to refactor!!!!
 app.post("/register", (req,res) => {
+   // check email
+   if (req.body.email.length <= 0 || req.body.password.length <= 0) {
+    return res.send("please fill in everything!");
+  }  
+   if (checkEmailExists(users, req.body.email)) {
+    return res.send("email already exists!");
+  }
+
   let generatedId = generateRandomString();
   users[generatedId] = {
     "id" : generatedId,
     "email": req.body.email,
-    "password": req.body.password
+    "password": bcrpyt.hashSync(req.body.password, salt)
   };
-  // check email
-  if (req.body.email.length <= 0 || req.body.password.length <= 0) {
-    res.statusCode = 400;
-    res.write("please fill in everything!");
-    res.end();
-  } else if (sortObj(users, req.body.email) === false) {
-    res.statusCode = 400;
-    res.write("email already exists!");
-    res.end()
-  } else {
+ 
   res.cookie("user_id", users[generatedId].id);
+  // req.session.users[generatedId].id = username;
   res.redirect("/urls")
-  }
+  console.log("NEWEST CEHCK *************")
+  console.log(users)
 });
+
+//validating log in
+app.post("/sign_in", (req, res) => {
+
+  if (req.body.email.length <= 0 || req.body.password.length <= 0) {
+    return res.send("please fill in everything!");
+  }
+  if (!checkEmailExists(users, req.body.email)) {
+    return res.send("information is incorrect!")
+  }
+  if (checkEmailExists(users, req.body.email)) {
+    if (!checkPassword(users, req.body.password)) {
+      return res.send("information is incorrect")
+    } 
+  }
+  res.cookie("user_id", users[req.cookies["user_id"]]);
+  res.redirect("/urls")
+})
 
 // the method to login and store a cookie
 app.post("/login", (req, res) => {
   res.redirect("/register")
 });
+
+app.post("/signin", (req, res) => {
+  res.redirect("/sign_in")
+});
+
 
 //make it logout
 app.post("/logout", (req, res) => {
